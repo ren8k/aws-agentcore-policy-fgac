cedb775e06a31b360616aa1d9ce863f68ea7b134
renya <rsmilek5492@gmail.com>
2026-01-11 16:30:59 +0900

    refactor: use native L1 interceptorConfigurations property
    - Upgrade aws-cdk-lib from 2.232.2 to 2.234.1
    - Replace addPropertyOverride workaround with native interceptorConfigurations L1 property support
    - Update stack name to GatewayInterceptorStack002 for new deployment

--- cdk-agentcore-gw-interceptors/bin/gateway-interceptors.ts
+++ cdk-agentcore-gw-interceptors/bin/gateway-interceptors.ts
@@ -3,7 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { GatewayInterceptorStack } from "../lib/gateway-interceptors-stack";
 
 const app = new cdk.App();
-new GatewayInterceptorStack(app, "GatewayInterceptorStack001", {
+new GatewayInterceptorStack(app, "GatewayInterceptorStack002", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
